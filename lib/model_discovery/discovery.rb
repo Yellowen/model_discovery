57b495e941156e261498181cfe19258bc1b49d01
Sameer Rahmani <lxsameer@gnu.org>
2014-02-01 11:58:08 +0330

    Bundler group requirement bug fixed, v0.2.1

--- lib/model_discovery/discovery.rb
+++ lib/model_discovery/discovery.rb
@@ -1,13 +1,15 @@
 module ModelDiscovery
   def self.build_table_list
     # Get all gem by requiring them
-    all_gems = Bundler.require
+    all_gems = Bundler.require()
 
     # Discover all model files in gem files and load them
     all_gems.each do |gem|
-      puts "Gem name: #{gem.name}"
-      spec = Gem::Specification.find_by_name gem.name
-      discover spec.gem_dir
+      if gem.groups.include? Rails.env.to_sym or gem.groups.include? :default
+        puts "Gem name: #{gem.name}"
+        spec = Gem::Specification.find_by_name gem.name
+        discover spec.gem_dir
+      end
     end
 
     # Discover models in current rails app and load them
